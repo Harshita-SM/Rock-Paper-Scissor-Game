d37f517f38244a6c0bd25f5cd1d2aebd5b83a35c
Harshita <Harshita12280@gmail.com>
2026-04-08 21:07:07 +0530

    Implemented winner decision logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,37 @@ import './App.css'
 function App() { 
   let [playerMove , setPlayerMove] = useState("");
 
+const getComputerMove = () => {
+  const moves = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return moves[randomIndex];
+};
+
+
+const handleClick = (move) => {
+  setPlayerMove(move);
+  const compMove = getComputerMove();
+  console.log("Computer:", compMove);
+};
+
+const decideWinner = (player, computer) => {
+  if (player === computer) return "Draw";
+
+  if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    return "You Win";
+  }
+
+  return "Computer Wins";
+};
 
   return (
     <div>
       <h1>Computer : User</h1>
-      <button onClick = {() => setPlayerMove("rock")}>Rock 🪨</button>
+      <button onClick = {() => handleClick("rock")}>Rock 🪨</button>
       <button>onClick = {() => setPlayerMove("Paper")}Paper 📄</button>
       <button>onClick = {() => setPlayerMove("Scissor")}Scissor ✂️</button>
        <p>Your Move: {playerMove}</p>
